150ae986566960184ca8f3a50ff951a3557e876c
JiwonJang01 <fx567849@gmail.com>
2024-06-22 21:45:37 +0900

    setting

---
+++ src/main/java/com/wordgarden/wordgarden/WordgardenApplication.java
@@ -0,0 +1,13 @@
+package com.wordgarden.wordgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordgardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordgardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordgarden/wordgarden/WordgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordgarden.wordgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordgardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
