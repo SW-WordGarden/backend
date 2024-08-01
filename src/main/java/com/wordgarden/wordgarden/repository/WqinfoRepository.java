04d6d4a85c01f9c353c517f51b89b548a7a89d30
unknown <fx567849@gmail.com>
2024-08-01 12:10:24 +0900

    Feat: add Repositories

---
+++ src/main/java/com/wordgarden/wordgarden/repository/WqinfoRepository.java
@@ -0,0 +1,13 @@
+package com.wordgarden.wordgarden.repository;
+
+import com.wordgarden.wordgarden.entity.Wqinfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WqinfoRepository extends JpaRepository<Wqinfo, String> {
+    List<Wqinfo> findByWordWordId(String wordId);
+
+}
---
+++ src/main/java/com/wordgarden/wordgarden/repository/WqwrongRepository.java
@@ -0,0 +1,13 @@
+package com.wordgarden.wordgarden.repository;
+
+
+import com.wordgarden.wordgarden.entity.Wqwrong;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WqwrongRepository extends JpaRepository<Wqwrong, Long> {
+    List<Wqwrong> findByUserUid(String uid);
+}
