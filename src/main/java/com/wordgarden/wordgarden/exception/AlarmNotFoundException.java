2f7fd69dd06399acdd842d64889b3746791e4aae
unknown <fx567849@gmail.com>
2024-08-21 16:20:29 +0900

    update: sharing & alram

---
+++ src/main/java/com/wordgarden/wordgarden/exception/AlarmNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wordgarden.wordgarden.exception;
+
+public class AlarmNotFoundException extends RuntimeException {
+    public AlarmNotFoundException(String message) {
+        super(message);
+    }
+}
